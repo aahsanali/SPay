e0769c4cf87c368f38a7fa713453ddada332d253
Naveed Ahsan <naveed.ahsan@coeus-solutions.de>
2023-03-16 14:37:52 +0100

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,8 +1,7 @@
 // swift-tools-version: 5.7
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
-import PackageDescription
-
+let pkgName = "SmartPay"
 let url = "https://raw.githubusercontent.com/aahsanali/SPayOnline/frameworks/sp/1.1/SmartPay.xcframework.zip"
 let checkSum = "d7c1a0999672c1e792681766297d1f91a0c421ac4ff37d1424518b267213b1f4"
 let package = Package(
@@ -12,7 +11,9 @@ let package = Package(
             name: pkgName,
             targets: [pkgName]),
     ],
-    dependencies: [.package(url: "https://github.com/aahsanali/KCloud.git", from:  "1.0.0")],
+    dependencies: [
+        .package(url: "https://github.com/aahsanali/KCloud.git", from:  "1.0.0")
+    ],
     targets: [
         .binaryTarget(name: pkgName, url: url, checksum: checkSum)
     ]
